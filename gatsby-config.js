661192b70dd64269db3191d909065cc1d86c5c6f
AjithAj10 <ajithraveendranr@gmil.com>
2024-09-28 14:40:29 +0530

    config files

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,30 +6,44 @@
  const env = require("./config/environment");
  //for 0.9 priority
  const priorityPages = ["/marketing-launchpad/digital-marketing-course/"];
- 
-    //  {
-    //    resolve: `gatsby-plugin-facebook-pixel`,
-    //    options: {
-    //      pixelId: "254822124949497",
-    //    },
-    
-    
-     // {
-     //   resolve: `gatsby-transformer-remark`,
-     //   options: {
-     //     // Footnotes mode (default: true)
-     //     footnotes: true,
-     //     // GitHub Flavored Markdown mode (default: true)
-     //     gfm: true,
-     //     // Plugins configs
-     //     plugins: [],
-     //   },
-     // },
-    //  {
-      //  resolve: `gatsby-plugin-linkedin-insight`,
-      //  options: {
-      //    partnerId: `362443`,
-   
-     //     // Include LinkedIn Insight in development.
-     //     // Defaults to false meaning LinkedIn Insight will only be loaded in production.
- 
+
+ module.exports = {
+  plugins: [
+    `gatsby-plugin-sass`,
+     `gatsby-plugin-react-helmet`,
+     `gatsby-plugin-image`,
+     `gatsby-plugin-sharp`,
+     {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+       host: "https://www.kraftshala.com",
+       sitemap: "https://www.kraftshala.com/sitemap/sitemap-0.xml",
+        policy: [{ userAgent: "*", allow: "/" }],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+         //  "UA-102655297-1", // Google Analytics / GA,
+         //  "G-BGCZN1ZFJ7"
+        ],
+      },
+    },
+
+    {
+      resolve: "gatsby-plugin-zopfli",
+      options: {
+        extensions: ["css", "html", "js", "svg"],
+       },
+     },
+     {
+      resolve:"gatsby-plugin-mdx",
+      options:{
+        extensions:[".mdx",".md"],
+        mediaTypes: [`text/markdown`, `text/x-markdown`],
+      }
+    },
+  ],
+}
